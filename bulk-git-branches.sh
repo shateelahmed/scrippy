65181c64877b54253361af37c7deec0d545be3c8
Md Yousuf Ali <yousuf3usb@gmail.com>
2024-08-13 11:39:03 +0600

    refactor: wrap with quotes

--- bulk-git-branches.sh
+++ bulk-git-branches.sh
@@ -28,7 +28,7 @@ green=$(tput setaf 2)
 reset=$(tput sgr0)
 
 # Iterate over immediate child directories
-for dir in in $(ls -d $target_directory/*/); do
+for dir in in $(ls -d "$target_directory"/*/); do
   if [ -d "$dir" ]; then
     if is_git_repo "$dir"; then
       branch=$(git -C "$dir" branch --show-current)
