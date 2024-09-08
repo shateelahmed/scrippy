acadc005ae69fac29cb7f774ee6d5798b08d7a6a
Shateel Ahmed <shateel.ahmed@brainstation-23.com>
2024-09-08 22:45:49 +0600

    refactor: remove script location from load-env as it's already set in calling script

--- load-env.sh
+++ load-env.sh
@@ -1,4 +1,3 @@
-script_location="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 env_file_location="$script_location/.env"
 if [ -f "$env_file_location" ]; then # set ENV varaibles from .env file if it exists
     set -o allexport
