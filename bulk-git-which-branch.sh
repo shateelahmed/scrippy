1a063249eff2427232a3a6dadc3e8da290a4f8af
Shateel Ahmed <shateel.ahmed@brainstation-23.com>
2024-10-14 11:47:52 +0600

    refactor: refine structure

--- bulk-git-which-branch.sh
+++ bulk-git-which-branch.sh
@@ -31,19 +31,20 @@ done
 source $script_location/load-env.sh
 source $script_location/target-directory.sh
 source $script_location/verify-git-repo.sh
-
-# Get terminal color codes
-green=$(tput setaf 2)
-reset=$(tput sgr0)
+source $script_location/directory-name.sh
+source $script_location/terminal-color-codes.sh
 
 echo "Target directory: $target_directory"
 
 # Iterate over immediate child directories
-for child_directory in in $(ls -d "$target_directory"/*/); do
-    if is_git_repo "$child_directory"; then
-        # child_directory_name will contain only the name of the child directory; not the full path
-        child_directory_name="$(basename "${child_directory%/*}")"
-        current_branch=$(git -C "$child_directory" branch --show-current)
-        echo "$child_directory_name - ${green}$current_branch${reset}"
+for child_directory in $(ls -d $target_directory/*/); do # iterate over each directory
+    if ! is_git_repo "$child_directory"; then
+        # condition 1: current child_directory is not a git repo
+
+        continue
     fi
+
+    child_directory_name=$(get_directory_name "$child_directory")
+    current_branch=$(git -C "$child_directory" branch --show-current)
+    echo "$child_directory_name - ${green}$current_branch${reset}"
 done
